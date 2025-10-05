3cd90d1402a4b0d1d2c7773b8004c26a57e48bf5
gustvohnsv <guvi10vieira@gmail.com>
2025-10-05 12:14:51 -0300

    Voltei o navegador de testes para Chrome

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,10 +1,16 @@
-Capybara.register_driver :headless_firefox do |app|
-  options = Selenium::WebDriver::Firefox::Options.new
-  options.add_argument('--headless')
+require 'capybara'
+require 'selenium-webdriver'
 
-  Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1920,1080')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
-Capybara.javascript_driver = :headless_firefox
-
+Capybara.javascript_driver = :headless_chrome
 Capybara.default_max_wait_time = 10
