192a97c6747cedaa4b60ca8439d33bb10619771d
Kaique de Jesus Pessoa Santos <kaique10defevereiro@gmail.com>
2025-10-05 17:22:20 -0300

    capybara: auto-detect chromium/chrome binary and prefer local chromedriver

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -2,8 +2,20 @@ require 'capybara'
 require 'selenium-webdriver'
 
 Capybara.register_driver :headless_chrome do |app|
-  chrome_bin_path = ENV['CHROME_BIN'] || '/usr/bin/google-chrome'
-  driver_path = ENV['CHROMEDRIVER_PATH'] || '/usr/bin/chromedriver'
+  # auto-detect Chrome/Chromium binary in common locations (prefer snap), or use ENV override
+  chrome_bin_path = ENV['CHROME_BIN'] || [
+    '/snap/bin/chromium',
+    '/usr/bin/chromium',
+    '/usr/bin/chromium-browser',
+    '/usr/bin/google-chrome',
+    '/opt/google/chrome/chrome'
+  ].find { |p| File.exist?(p) }
+
+  # allow explicit override for chromedriver path; otherwise detect common locations
+  driver_path = ENV['CHROMEDRIVER_PATH'] || [
+    '/usr/bin/chromedriver',
+    '/usr/local/bin/chromedriver'
+  ].find { |p| File.exist?(p) }
 
   options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
     opts.add_argument('--headless=new')
@@ -14,12 +26,13 @@ Capybara.register_driver :headless_chrome do |app|
     opts.add_argument('--disable-site-isolation-trials')
     opts.add_argument('--disable-features=IsolateOrigins,site-per-process')
 
-    opts.binary = chrome_bin_path
+    opts.binary = chrome_bin_path if chrome_bin_path
   end
 
-  service = if File.exist?(driver_path)
+  service = if driver_path && File.exist?(driver_path)
     Selenium::WebDriver::Service.chrome(path: driver_path)
   else
+    # Let webdrivers manage it if available; otherwise Selenium will try default
     Selenium::WebDriver::Service.chrome
   end
 
