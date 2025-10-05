3a29f3dae5d5c920b1dbb43c45ecdb2cb91ded7f
Joauww16 <jprbarbosa16@gmail.com>
2025-10-05 15:47:51 -0300

    Teste final

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  #get 'profiles/edit'
-  #get 'profiles/update'
   get 'documento/show'
   get 'perfil/show'
 
