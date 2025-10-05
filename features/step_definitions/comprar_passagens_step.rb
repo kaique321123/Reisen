8294568297bf7cc2385977a64032b80f2a74e8b2
gustvohnsv <guvi10vieira@gmail.com>
2025-10-05 12:28:51 -0300

    Modifiquei os step para o ER-18 : Comprar passagem

--- features/step_definitions/comprar_passagens_step.rb
+++ features/step_definitions/comprar_passagens_step.rb
@@ -9,6 +9,7 @@ end
 
 Quando('eu clico no link da companhia {string}') do |company_name|
   click_link(company_name)
+  sleep 0.5
 end
 
 Então('a nova aba da companhia {string} deve ser aberta e ter o URL validado') do |company_name|
@@ -18,29 +19,42 @@ Então('a nova aba da companhia {string} deve ser aberta e ter o URL validado') 
     "Azul" => "/mock/azul"
   }
   expected_url = companies[company_name]
-  new_window = windows.last
-  switch_to_window(new_window)
-  expect(page).to have_current_path(expected_url, wait: 1, ignore_query: true)
+
+  expect(page.windows.size).to be > 1, "Nova janela não foi aberta"
+
+  within_window(page.windows.last) do
+    expect(page).to have_current_path(expected_url, wait: 5, ignore_query: true)
+  end
+
+  page.windows.last.close
 end
 
 Quando('eu clico no link de uma delas') do
   link = find('a[href^="/mock"]', match: :first)
   @texto = link.text
   click_link(@texto)
+  sleep 0.5
 end
 
 E('eu clico no link de uma delas {string}') do |string|
   link = find('a[href^="/mock"]', match: :first)
+
   page.execute_script("arguments[0].href = '/mock/erro/';", link)
   link.click
-  if windows.size > 1
-    new_window = windows.last
-    switch_to_window(new_window)
+  sleep 0.5
+
+  if page.windows.size > 1
+    within_window(page.windows.last) do
+      # Só preciso ser redirecionado para a página de erro
+    end
   end
 end
 
 Então('eu devo ver uma mensagem de erro') do
-  expect(page).to have_content('Erro')
+  within_window(page.windows.last) do
+    expect(page).to have_content('Erro', wait: 5)
+  end
+  page.windows.last.close if page.windows.size > 1
 end
 
 Então('a nova aba da companhia deve ser aberta e ter o URL validado') do
@@ -49,12 +63,16 @@ Então('a nova aba da companhia deve ser aberta e ter o URL validado') do
     "Gol" => "/mock/gol",
     "Azul" => "/mock/azul"
   }
-  new_window = windows.last
-  switch_to_window(new_window)
-  expect(page).to satisfy do |p|
-    companies.values.any? do |expected_url|
-      p.has_current_path?(expected_url, wait: 1, ignore_query: true)
+
+  expect(page.windows.size).to be > 1, "Nova janela não foi aberta"
+
+  within_window(page.windows.last) do
+    url_valida = companies.values.any? do |expected_url|
+      page.has_current_path?(expected_url, wait: 5, ignore_query: true)
     end
+
+    expect(url_valida).to be_truthy, "URL não corresponde a nenhuma companhia esperada. URL atual: #{current_path}"
   end
 
+  page.windows.last.close
 end
