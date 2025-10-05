273be0196a352b193e2cb96b0cb1f332d9167838
gustvohnsv <guvi10vieira@gmail.com>
2025-10-05 12:08:30 -0300

    Mudei o navegador de testes de Chrome para Firefox

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,15 +1,10 @@
-Capybara.register_driver :headless_chrome do |app|
-  options = Selenium::WebDriver::Chrome::Options.new
-
+Capybara.register_driver :headless_firefox do |app|
+  options = Selenium::WebDriver::Firefox::Options.new
   options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--disable-gpu')
-  options.add_argument('--window-size=1280,800')
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  Capybara::Selenium::Driver.new(app, browser: :firefox, options: options)
 end
 
-Capybara.javascript_driver = :headless_chrome
+Capybara.javascript_driver = :headless_firefox
 
 Capybara.default_max_wait_time = 10
